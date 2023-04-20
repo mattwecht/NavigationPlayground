4a84e558917da845ee8a6e34f2dfa6d2fe310721
Matt Wecht <mattwecht@gmail.com>
2023-04-20 17:03:19 -0400

    Initial Commit

---
+++ NavigationPlayground/NavigationPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPlaygroundApp.swift
+//  NavigationPlayground
+//
+//  Created by scholar on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
